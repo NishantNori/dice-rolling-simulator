7f5adb7df511d2ef10b97fd9445728a956ee15b1
Nishant Nori <nishantnori153@gmail.com>
2017-09-18 21:26:00 +0530

    Add madLibsGenerator.py

---
+++ madLibsGenerator.py
@@ -0,0 +1,17 @@
+# Once there was a boy. He wanted to become <Harry Potter>. He was waiting for 
+# call from <Hogwarts>. But it never came. So he self-taught <magic>, created a 
+# <wand> out of a <Dragon> <Heart String> and became very powerful. Using his <wand>, he created 
+# <panic> among the people.
+
+harryPotter = input("Enter a famous person's name:\n")
+hogwarts = input("Name of a university:\n")
+magic = input("His favorite subject:\n")
+wand = input("His favorite apparatus:\n")
+dragon = input("An animal:\n")
+heartString = input("A body part:\n")
+panic = input("A feeling:\n")
+
+print("Once there was a boy. He wanted to become " + harryPotter + ". He was waiting for a\
+ call from " + hogwarts + ". But it never came. So he self-taught " + magic + ", created a "
+ + wand + " out of a " + dragon + " " + heartString + " and became very powerful. Using his " + wand + ", he created "
+ + panic + " among the people.\n")
